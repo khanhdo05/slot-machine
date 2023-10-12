2a06269f05cc89af5f0a54fcde2cb14b845354e4
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-10-12 12:02:22 -0500

    correct display of rows and columns

--- main.py
+++ main.py
@@ -37,9 +37,10 @@ def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) -1:
-                print(column[row], "|")
+                print(column[row], end=" | ")
             else:
-                print(column[row])
+                print(column[row], end="")
+        print()
 
 def deposit():
     while True:

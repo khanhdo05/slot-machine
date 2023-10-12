4a8a778791c124366ebf91c36a9338e442fc8ae6
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-10-12 10:59:27 -0500

    slot machine variables

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ MIN_BET = 1
 ROWS = 3
 COLS = 3
 
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    
+
 def deposit():
     while True:
         amount = input("How much would you like to deposit: $")
